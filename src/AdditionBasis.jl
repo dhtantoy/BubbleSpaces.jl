7b94bf1865abf144903c0da0b0c1d8c97fc5a9f4
dhtan <dinghangtan@gmail.com>
2024-09-24 16:13:33 +0800

    fix bug for `AdditionBasis * Float`

--- src/AdditionBasis.jl
+++ src/AdditionBasis.jl
@@ -4,10 +4,11 @@ struct AdditionBasis{T1, T2}
 end
 Gridap.:+(dv1::MultiField.MultiFieldFEBasisComponent, dv2::MultiField.MultiFieldFEBasisComponent) = AdditionBasis(dv1, dv2)
 
-Gridap.dot(f, a::AdditionBasis) = dot(f, a.dv1) + dot(f, a.dv2)
-Gridap.dot(a::AdditionBasis, f) = dot(f, a)
-for op in (:inner, :dot)
-    @eval Gridap.$op(a::AdditionBasis, b::AdditionBasis) = $op(a.dv1, b.dv1) + $op(a.dv2, b.dv2) + $op(a.dv1, b.dv2) + $op(a.dv2, b.dv1)
+
+for op in (:inner, :dot, :*)
+    @eval Gridap.$op(a::AdditionBasis, f) = $op(f, a)
+    @eval Gridap.$op(f, b::AdditionBasis) = $op(f, b.dv1) + $op(f, b.dv2) 
+    @eval Gridap.$op(a::AdditionBasis, b::AdditionBasis) = $op(a.dv1, b) + $op(a.dv2, b)
 end
 for op in (:gradient,)
     @eval Gridap.$op(a::AdditionBasis) = $op(a.dv1) + $op(a.dv2)
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,7 @@ using Gridap
 using Gridap.ReferenceFEs
 using Test
 
-
+μ((x, y)) = sin(x)*cos(y)
 N = 10;
 m = CartesianDiscreteModel((0, 1, 0, 1), (N, N)) |> simplexify
 labels = get_face_labeling(m)
@@ -25,7 +25,7 @@ P = TrialFESpace(Q)
 Xb = MultiFieldFESpace([U_trial, B_trial, P])
 Yb = MultiFieldFESpace([U_test, B_test, Q])
 
-@inline a(u, v) = ∫(∇(u)⊙∇(v))dx 
+@inline a(u, v) = ∫(∇(u)⊙∇(v)*μ)dx 
 @inline b(u, p) = ∫(∇⋅u * p)dx
 
 @inline A((u, p), (v, q)) = a(u, v) + b(v, p) + b(u, q)
